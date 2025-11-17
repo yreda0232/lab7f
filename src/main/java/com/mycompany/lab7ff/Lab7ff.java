b77a02f35c7a1f8f28eb3b04313e09434b38a8fd
youssef reda <yreda0232@gmail.com>
2025-11-17 03:22:08 +0200

    .

---
+++ src/main/java/com/mycompany/lab7ff/Lab7ff.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lab7ff;
+
+/**
+ *
+ * @author youssef
+ */
+public class Lab7ff {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
